c6a7b72686f3862239c29944f6e9973572c7192c
chunga123 <nguyenthevinh1002@gmail.com>
2020-07-16 10:19:11 +0700

    Updates

---
+++ precache-manifest.08e3c82d3596396bfe66d1f719f177a1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660b490ef621c98b48dd3f06e0405a68",
+    "url": "/THTs/index.html"
+  },
+  {
+    "revision": "7ef5400873c5a54a7347",
+    "url": "/THTs/static/css/main.6d00fbfd.chunk.css"
+  },
+  {
+    "revision": "ca23486943a20b52f131",
+    "url": "/THTs/static/js/2.c4adb878.chunk.js"
+  },
+  {
+    "revision": "959444e7bc5405626d18ae4555c095a3",
+    "url": "/THTs/static/js/2.c4adb878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ef5400873c5a54a7347",
+    "url": "/THTs/static/js/main.af707efb.chunk.js"
+  },
+  {
+    "revision": "9be59554ce9654279a7b",
+    "url": "/THTs/static/js/runtime-main.0d9edff5.js"
+  },
+  {
+    "revision": "575e4317521b381ac94c0c8207c81979",
+    "url": "/THTs/static/media/Raleway-Bold.575e4317.ttf"
+  },
+  {
+    "revision": "430a0518f5ff3b6c8968b759a29b36e2",
+    "url": "/THTs/static/media/Raleway-Medium.430a0518.ttf"
+  },
+  {
+    "revision": "580d0778ad254335be45bf58bb449f43",
+    "url": "/THTs/static/media/Raleway-Regular.580d0778.ttf"
+  },
+  {
+    "revision": "17ba6410cbc694808961a988fd4426de",
+    "url": "/THTs/static/media/Raleway-SemiBold.17ba6410.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/THTs/static/media/logo.5d5d9eef.svg"
+  }
+]);
